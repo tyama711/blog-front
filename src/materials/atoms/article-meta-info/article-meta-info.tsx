d3ae150659e7ec995a1313b22aeb1c5d9152d972
Takuya Yamasaki <takuyaya@yahoo-corp.jp>
2019-06-20 00:43:56 +0900

    refactoring

--- src/materials/atoms/article-meta-info/article-meta-info.tsx
+++ src/materials/atoms/article-meta-info/article-meta-info.tsx
@@ -11,9 +11,9 @@ interface ArticleMetaInfoProps {
 export default function ArticleMetaInfo(props: ArticleMetaInfoProps) {
   return (
     <div className="article-meta-info">
-      {props.updateDate
-        ? "Updated " + dateFormat(props.updateDate, "yyyy/mm/dd HH:MM")
-        : "Posted " + dateFormat(props.createDate, "yyyy/mm/dd HH:MM")}
+      {dateFormat(props.createDate, "yyyy/mm/dd")}
+      {props.updateDate &&
+        ` (Updated ${dateFormat(props.updateDate, "yyyy/mm/dd")})`}
     </div>
   );
 }
